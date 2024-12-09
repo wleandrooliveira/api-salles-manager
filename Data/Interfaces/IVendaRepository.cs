7174e2b1fa5c9830ec0472618d5f3dc0afa60102
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2024-12-09 02:01:59 -0300

    interfaces and factory

---
+++ Data/Interfaces/IVendaRepository.cs
@@ -0,0 +1,12 @@
+﻿using api_salles_manager.Domain;
+
+namespace api_salles_manager.Data.Interfaces
+{
+    public interface IVendaRepository
+    {
+        Task<Venda?> GetByIdAsync(Guid id);
+        Task AddAsync(Venda venda);
+        Task UpdateAsync(Venda venda);
+        Task DeleteAsync(Venda venda);
+    }
+}
---
+++ Data/VendasDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_salles_manager.Data
+{
+    public class VendasDbContextFactory : IDesignTimeDbContextFactory<VendasDbContext>
+    {
+        public VendasDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<VendasDbContext>();
+            optionsBuilder.UseNpgsql("Host=localhost;Database=db_vendas;Username=postgres;Password=Quimica@7295");
+
+            return new VendasDbContext(optionsBuilder.Options);
+        }
+    }
+}
